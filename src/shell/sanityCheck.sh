2e7e44478bc2b85ceffeeb95cfd6d290bdf58ff5
esayyari <esayyari@eng.ucsd.edu>
2015-09-21 17:59:59 -0700

    change the quartettable method

--- src/shell/sanityCheck.sh
+++ src/shell/sanityCheck.sh
@@ -5,14 +5,14 @@ source $DIR/setup.sh
 source $WS_HOME/global/src/shell/setup.sh
 sp=11
 $WS_GLB_BIN/quart_bin genTree $sp > $WS_LOC_TS/testSanity/test.nwk;
-for i in {1..5}; do
+for i in {1..100}; do
 	echo "">>$WS_LOC_TS/testSanity/test.nwk;
 	echo "`head -n 1 $WS_LOC_TS/testSanity/test.nwk`">> $WS_LOC_TS/testSanity/test.nwk;
 done
-#for i in {1..400}; do
-#	echo "">>$WS_LOC_TS/testSanity/test.nwk;
-#	$WS_GLB_BIN/quart_bin genTree $sp >> $WS_LOC_TS/testSanity/test.nwk;
-#done
+for i in {1..900}; do
+	echo "">>$WS_LOC_TS/testSanity/test.nwk;
+	$WS_GLB_BIN/quart_bin genTree $sp >> $WS_LOC_TS/testSanity/test.nwk;
+done
 
 $DIR/summarize.tree.sh -w 1 -s fm -d prod -r $WS_LOC_TS/testSanity  $WS_LOC_TS/testSanity/test.nwk;
 echo "";
--- src/utils/quartetTable.py
+++ src/utils/quartetTable.py
@@ -1,21 +1,36 @@
 import sys
+import re
 frq=dict()
 for line in sys.stdin:
-    trip=line.replace("|","").split()
-    sortedTrip=sorted(trip)
-    k = "/".join(sortedTrip)
-    keyt = sorted(line.split(" | "))
-    keym= sorted(keyt[0].split(" "))
-    
-	
-    
-    v = frq.get(k, dict (zip(trip[1:4],[0]*3)))
-    v[keym[1]] += 1
-    frq[k] = v
+	posPat=re.match("alpha|gamma|beta",line) 
+    	if posPat is not None:
+	    line=re.sub("alpha |gamma |beta ","",line)
+	    trip=line.replace("|","").split()
+	    sortedTrip=sorted(trip)
+	    k = "/".join(sortedTrip)
+	    keyt = sorted(line.split(" | "))
+	    keym= sorted(keyt[0].split(" "))
+	    
+		
+	    
+	    v = frq.get(k, dict (zip(trip[1:4],[1]*3)))
+	    v[keym[1]] += 1
+	    frq[k] = v
+	else:
+	    line = re.sub("delta |epsilon ","",line)
+            k = "/".join(sorted(line.split()))
+	    trip = sorted(k.split("/"))
+            v = frq.get(k, dict (zip(trip[1:4],[1]*3)))
+            v[trip[1]] += 1
+	    v[trip[2]] += 1
+	    v[trip[3]] += 1
+            frq[k] = v
+
 
 for k in sorted(frq.keys()):
     print k,
     tf = frq[k]
     for t in sorted(tf.keys()):
-        print tf[t],
+	print tf[t],
     print
+
