a7f7b784757ad79cd3b62825c8cc583a570cde97
Erfan Sayyari <esayyari@login01.osgconnect.net>
2015-09-28 21:43:12 -0500

    a

--- src/shell/sanityCheck.sh
+++ src/shell/sanityCheck.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-
+module load java
+module load python/2.7
+module load all-pkgs
+module load gcc
 export DIR=`pwd`
 source $DIR/setup.sh
 source $WS_HOME/global/src/shell/setup.sh
 sp=11
 $WS_GLB_BIN/quart_bin genTree $sp > $WS_LOC_TS/testSanity/test.nwk;
-for i in {1..100}; do
+for i in {1..1000}; do
 	echo "">>$WS_LOC_TS/testSanity/test.nwk;
 	echo "`head -n 1 $WS_LOC_TS/testSanity/test.nwk`">> $WS_LOC_TS/testSanity/test.nwk;
 done
-for i in {1..900}; do
+for i in {1..10}; do
 	echo "">>$WS_LOC_TS/testSanity/test.nwk;
 	$WS_GLB_BIN/quart_bin genTree $sp >> $WS_LOC_TS/testSanity/test.nwk;
 done
 
-$DIR/summarize.tree.sh -w 1 -s fm -d prod -r $WS_LOC_TS/testSanity  $WS_LOC_TS/testSanity/test.nwk;
+$DIR/summarize.tree.sh -w 1 -s fm -d min -r $WS_LOC_TS/testSanity  $WS_LOC_TS/testSanity/test.nwk;
 echo "";
 echo "`head -n 1 $WS_LOC_TS/testSanity/distance.d_fastme_tree.nwk`";
 
--- src/utils/minDistance.py
+++ src/utils/minDistance.py
@@ -3,6 +3,7 @@ import sys
 
 def minDistance(frq):
 	mapDict = dict()
+	numPc = dict()
         v = set([0, 1, 2, 3])
         for k in sorted(frq.keys()):
                 d = sorted(k.split('/'))
@@ -13,28 +14,43 @@ def minDistance(frq):
 
                         if distKey not in mapDict.keys()  :
 				mapDict[distKey] = list()
-				p = np.log(min(1./3,1.*frq[k][d[i]]/(1.*sz)))
+				numPc[distKey] = 0
+				if np.abs(frq[k][d[i]]) < 5: 
+					numPc[distKey] += 1
+				p = np.log(min(1.,3.*frq[k][d[i]]/(1.*sz)))
                                 mapDict[distKey].append(p)
                         else:
-				p = np.log(min(1./3,1.*frq[k][d[i]]/(1.*sz)))
+				if np.abs(frq[k][d[i]]) < 5:
+					numPc[distKey] += 1
+				p = np.log(min(1.,3.*frq[k][d[i]]/(1.*sz)))
                                 mapDict[distKey].append(p)
                         s = set([0,i])
                         g = sorted(list(v - s))
                         distKey = d[g[0]]+ ' ' +d[g[1]]
 
                         if distKey not in mapDict.keys():
+				numPc[distKey] = 0
+                                if np.abs(frq[k][d[i]]) < 5: 
+                                        numPc[distKey] += 1
 				mapDict[distKey] = list()
-				p = np.log(min(1./3,1.*frq[k][d[i]]/(1.*sz)))
+				p = np.log(min(1.,3.*frq[k][d[i]]/(1.*sz)))
                                 mapDict[distKey].append(p)
                         else:
-				p = np.log(min(1./3,1.*frq[k][d[i]]/(1.*sz)))
+				p = np.log(min(1.,3.*frq[k][d[i]]/(1.*sz)))
+				if np.abs(frq[k][d[i]]) < 5:
+                                        numPc[distKey] += 1
                                 mapDict[distKey].append(p)
 			retDict = dict()
 
 	for key in mapDict:
+#		ml = sorted(mapDict[key])
+#		m = np.mean( ml[0:int(sz/100.*2)+1] )
 		m = min(mapDict[key])
-		if m==np.log(1./3):
+		pc = numPc[key]
+		if m==np.log(1):
 			m = 0
+		elif pc>0:
+			m = pc*np.log(0.5/sz)
 		else: 	
 			m = min(mapDict[key])
 		retDict[key] = -m
