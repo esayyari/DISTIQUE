d0bbdce757c8586448ee283cc00500ad697ff79b
esayyari <esayyari@eng.ucsd.edu>
2015-10-01 12:12:00 -0700

    any?

--- src/R/plot.r
+++ src/R/plot.r
@@ -25,3 +25,5 @@ require(reshape2)
 newdata<-dcast(d, model + X_sites + X_taxa + X_genes ~ method, value.var="FN_rate")
 
 d<-read.csv('FN_rates_11-taxon.csv',header=TRUE,sep=",",col.names = c('model','Xtaxa','method','Xsites','Xgenes','replicate','FNrate'))
+
+qplot(Xsites,FNrate,data=d,color=interaction(method),geom=c("point","line"),fun.y=mean,stat='summary')+facet_grid(model~Xgenes,scales="free_y")+theme_bw()+theme(legend.position="bottom")+xlab("Number of genes")+stat_summary(geom="errorbar",fun.ymin=function(x) {mean(x)-sd(x)/sqrt(length(x))},fun.ymax = function(x) {mean(x)+sd(x)/sqrt(length(x))},width=5)
