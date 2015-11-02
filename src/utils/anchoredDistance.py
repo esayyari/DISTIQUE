b7bd3e6a2fb428123b9c81d9f182c5404b552963
esayyari <esayyari@eng.ucsd.edu>
2015-11-02 08:50:38 -0800

    adding supplementary

--- src/utils/anchoredDistance.py
+++ src/utils/anchoredDistance.py
@@ -4,6 +4,9 @@ import itertools
 import sys
 import os
 from readQuartetTable import readQuartetTable
+from printDistanceTableToFile import printDistanceTableToFile 
+
+
 from findAnchoredQuartetTable import findAnchoredQuartetTable
 import numpy as np
 def anchoredDistance(**kwargs):
@@ -23,7 +26,8 @@ def anchoredDistance(**kwargs):
 	else:
 		frq=findAnchoredQuartetTable(achs,gt)	
 	D = anchoredDistanceFromFrq(frq,achs)
-	keyDict = sorted(np.unique((" ".join(D.keys())).split(" ")));               	
+	keyDict = sorted(np.unique((" ".join(D.keys())).split(" ")));              
+	print "print distance table to file"	
 	printDistanceTableToFile(D,keyDict,outfile)
 	return 
 def anchoredDistanceFromFrq(frq,achs):
@@ -32,11 +36,13 @@ def anchoredDistanceFromFrq(frq,achs):
 		kt = sorted(k.split("/"))
 		if ((achs[0] in kt ) and( achs[1] in kt)):
 			s = sorted(list(set(kt)-{achs[0],achs[1]}))
+			key1 = s[0]+" "+s[1]
+			key2 = s[1]+" "+s[0]
 			if s[0]<achs[0]:
-				D[s[0]+' '+s[1]]=-np.log(frq[kt][s[1]])
-				D[s[1]+' '+s[0]]=D[s[0]+' '+s[1]]
+				D[key1]=-np.log(frq[k][s[1]])
+				D[key2]=D[key1]	
 			elif s[0]>achs[0]:
-				D[s[0]+' '+s[1]]=-np.log(frq[kt][achs[1]])	
-				D[s[1]+' '+s[0]]=D[s[0]+' '+s[1]]
+				D[key1]=-np.log(frq[k][achs[1]])	
+				D[key2]=D[key1]
 	return D		 
 			
--- src/utils/printDistanceTableToFile.py
+++ src/utils/printDistanceTableToFile.py
@@ -1,8 +1,9 @@
 def printDistanceTableToFile (mapDict , keyDict, outfile):
+	print outfile
 	f1 = open(outfile, 'w+')
 	l = len(keyDict)
 	norm = 1.
-	ps = 20.
+	ps = 5.
 	print >> f1, l
 	for i in range(0,l):
         	sp = keyDict[i]
@@ -11,10 +12,12 @@ def printDistanceTableToFile (mapDict , keyDict, outfile):
                 	if i==j:
 				b = 0.
                         	print >> f1, '%0.6f' % b,
+
                         	continue
                 	k = sorted([keyDict[j],keyDict[i]])
 			a=mapDict[k[0]+' '+k[1]]
 			a += ps
 			a /=norm
                 	print >> f1,'%0.6f' % a,
+
         	print >> f1 
