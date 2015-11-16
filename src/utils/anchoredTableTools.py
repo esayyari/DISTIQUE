9d918d91200ff119222b1783a0ed5ea7f7796ede
esayyari <esayyari@eng.ucsd.edu>
2015-11-16 00:26:58 -0800

    all

--- src/utils/anchoredTableTools.py
+++ src/utils/anchoredTableTools.py
@@ -151,3 +151,114 @@ def findAnchoredDistanceTable(achs,trees,taxa,out):
                         D[key2]=D[key1]
         return D
 	
+def resolvePolytomy(pathToTree,node,verbose):
+        src_fpath = os.path.expanduser(os.path.expandvars(pathToTree))
+        if not os.path.exists(src_fpath):
+                 sys.stderr.write('Not found: "%s"' % src_fpath)
+        tlist = dendropy.TreeList()
+        tlist = dendropy.TreeList.get(path=src_fpath,schema="newick")
+        sp_tree = tlist[0]
+        adjacent_list = set()
+        dict_children=dict()
+        tn = 0
+        for t in node.adjacent_nodes():
+                dict_children[t.label] = t
+                adjacent_list.add(t.label)
+        if node.parent_node is not None:
+                label = node.parent_node.label
+                filter = lambda taxon: True if taxon.label==label else False
+                nd = sp_tree.find_node_with_taxon(filter)
+                if nd is not None:
+                        sp_tree.reroot_at_edge(nd.edge, update_bipartitions=False)
+        stack = list()
+        for e in sp_tree.postorder_node_iter():
+                n = len(e.child_nodes())
+                if n > 0:
+                        tmp_next = str()
+                        t = node.insert_new_child(n+1)
+                        children = set(node.child_nodes())
+                        for i in range(0,n):
+                                if len(stack)>0:
+                                        tmp = stack.pop()
+                                        if dict_children[tmp]  in children:
+                                                node.remove_child(dict_children[tmp])
+                                        elif len(node.child_nodes())==1:
+                                                continue
+                                        t.add_child(dict_children[tmp])
+                                        tmp_next += tmp
+                        t.label = tmp_next
+                        dict_children[tmp_next] = t
+                        stack.append(tmp_next)
+                elif e.is_leaf():
+                        e_t = e.taxon.__str__()
+                        e_t = e_t.split("'")
+                        stack.append(e_t[1])
+        if verbose:
+
+                print "done"
+                return None
+        else:
+                print "done"
+                return None
+def findPolytomies(con_tree,taxa,anch):
+	to_resolve = dict()
+	maxPolyOrder = 0
+	tmp = list()
+	node_dict = dict()
+	anch = set(anch)
+	taxa = set(con_tree.leaf_nodes())
+	for e in con_tree.postorder_node_iter():
+		tmp_set = set()
+		ch_tmp = set(e.adjacent_nodes())
+		intersect = ch_tmp & anch
+		interLen = len(intersect)
+		
+		n = len(ch_tmp)
+		anch_to_retain = list()
+		if (n>3 and interLen = 0 ) or (n>4 and interLen = 1) or (n>5 and interLen = 2):
+			sz = n
+			maxPolyOrder = max(maxPolyOrder,sz)
+			v = dict()
+			
+			for c in e.child_nodes():
+				if len(tmp)>0:
+					t = tmp.pop()
+					if c.label in anch:
+						anch_to_retain.append(c.label)
+						continue
+					v[c.label] = node_dict[c.label]
+					if t in anch:
+						continue
+					tmp_set = tmp_set | t
+			tmp.append(tmp_set);
+			node_dict[e.label] = tmp_set
+			if len(taxa-tmp_set)>0 and e.parent_node.label not in anch:
+				t = taxa-tmp_set-anch
+				v[e.parent_node.label] = t
+			to_resolve[e] = v
+		else:
+			for i in range(0,n-1):
+				if len(tmp)>0:
+					a = set(tmp.pop())
+					if a in anch:
+						continue
+					tmp_set = tmp_set | a
+			if e.is_leaf() and e.label not in anch:
+				tmp_set.add(e)
+			if e.label not in anch:
+				node_dict[e.label] = tmp_set
+			tmp.append(tmp_set)
+
+
+	return (to_resolve,maxPolyOrder)
+def getTaxaList(taxaDict):
+        taxa = dict()
+        inv_taxa=dict()
+        taxa_list=dict()
+        n = len(taxaDict)
+        for key1,vals1 in taxaDict.iteritems():
+                v = list()
+                for t in vals1:
+                        k = t.taxon.__str__()
+                        kv = k.split("'")
+                        v.append(kv[1])
--- src/utils/tableManipulationTools.py
+++ src/utils/tableManipulationTools.py
@@ -276,3 +276,63 @@ def findQuartetTable(trees,origKeys,keyType,tmpPath,verbose):
 	if verbose:
 		print "The quartet table has been computed"
 	return frq
+def findTrueAverageTableAnchoring(frq,anch,list_taxa,method):
+	n = len(list_taxa)
+	n = len(set(list_taxa)-set(anch))	
+	anch = sorted(list(anch))
+	lst_taxa = list(list_taxa.keys())
+	TotalKey = dict()
+	for i in range(0,n):
+		for j in range(i+1,n):
+			for k in range(j+1,n):
+				for z in range(k+1,n):
+					for taxon_i in list_taxa[lst_taxa[i]]:
+						for taxon_j in list_taxa[lst_taxa[j]]:
+							for taxon_k in list_taxa[lst_taxa[k]]:
+								for taxon_z in list_taxa[lst_taxa[z]]:
+									keyt = "/".join(sorted([taxon_i,taxon_j,taxon_k,taxon_z]))
+									lab_taxon_i = taxon_i
+									lab_taxon_j = taxon_j
+									lab_taxon_k = taxon_k
+									lab_taxon_z = taxon_z
+									tmp_dict = dict()
+									tmp_dict[lst_taxa[i]] = lab_taxon_i
+									tmp_dict[lst_taxa[j]] = lab_taxon_j
+									tmp_dict[lst_taxa[k]] = lab_taxon_k
+									tmp_dict[lst_taxa[z]] = lab_taxon_z
+									key_orig = "/".join(sorted([lab_taxon_i,lab_taxon_j,lab_taxon_k,lab_taxon_z]))
+									l = sorted([lst_taxa[i],lst_taxa[j],lst_taxa[k],lst_taxa[z]])
+									key_inv = "/".join(l)
+									v = frq[key_orig]
+									v_inv = dict()
+									for q in range(1,4):
+										q1 = sorted([tmp_dict[l[0]],tmp_dict[l[q]]])
+										stmp = list(s-{q})
+										q2 = sorted([tmp_dict[l[stmp[0]]],tmp_dict[l[stmp[1]]]])
+										if q1[0]<q2[0]:
+											v_inv[l[q]] = v[q1[1]]	
+										else:
+											v_inv[l[q]] = v[q2[1]]
+									if key_inv in TotalKey:
+										vt = TotalKey[key_inv] 
+										for keyt in vt.keys():
+											vt[keyt].append(v_inv[keyt])
+									else:
+										vt = dict()
+										for q in v_inv:
+											vt[q] = list()
+											vt[q].append(v_inv[q])
+									TotalKey[key_inv] = vt
+									
+	TotalKeyf = dict()
+	for q,v in TotalKey.iteritems():
+		vtt = dict()
+		for q2,v2 in v.iteritems():
+			if method == "gmean":
+				vtt[q2] = stats.gmean(v2)
+			elif method == "mean":
+				vtt[q2] = mean(v2)
+			else:
+				vtt[q2] = sqrt(mean(square(v2)))
+		TotalKeyf[q] = vtt
+	return TotalKeyf
