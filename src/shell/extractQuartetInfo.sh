f5635b0af7ac94d322549c7797a63ab0f617dbdc
Erfan Sayyari <esayyari@eng.ucsd.edu>
2015-12-04 16:10:16 -0800

    all

---
+++ src/shell/extractQuartetInfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+test $# == 2 || { echo USAGE: $0 file_with_quartet_info output_file; exit 1; }
+
+DIR=$( cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
+cat $1 | grep -e '^{[0-9]' | grep -oe '\[.*' | sed -e 's/:.*//g' > "`echo $2`"-quadpartition
+
+cat $1 | grep -e '^{[0-9]' | sed -e 's/\[.*//g' >  "`echo $2`"-bipartition
+
+cat $1 | grep -e '^{[0-9]' | grep -oe ':.*' | sed -e 's/: //g' >  "`echo $2`"-posterior
