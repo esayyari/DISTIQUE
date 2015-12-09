8306d9e89128cccab988b21605764f3953e5871f
Erfan Sayyari <esayyari@tscc-login2.sdsc.edu>
2015-12-08 18:00:45 -0800

    all

--- src/shell/extractQuartetInfo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-test $# == 2 || { echo USAGE: $0 file_with_quartet_info output_file; exit 1; }
-
-DIR=$( cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
-cat $1 | grep -e '^{[0-9]' | grep -oe '\[.*' | sed -e 's/[{} ]//g' | sed -e 's/:.*//g' > "`echo $2`"-bipartition
-
-cat $1 | grep -oe '^{[0-9].*' | sed -e 's/\[.*//g' | sed -e 's/ //g'  >  "`echo $2`"-quadpartition
-
-cat $1 | grep -e '^{[0-9]' | grep -oe ':.*' | sed -e 's/ //g' | sed -e 's/://g' >  "`echo $2`"-posterior
--- src/shell/test_astral.sh
+++ src/shell/test_astral.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR_Astral=$WS_HOME/ASTRAL/Astral
-path_11=/oasis/projects/nsf/uot136/esayyari/data/mammalian
-out=/oasis/projects/nsf/uot136/esayyari/outputs/
+path_11=/oasis/projects/nsf/uot136/esayyari/data/avian
+out=/oasis/projects/nsf/uot136/esayyari/old-outputs/
 if [ -s $DIR/tasks.massive ]; then
 rm $DIR/tasks.massive
 fi
@@ -18,6 +18,6 @@ for file in `find $path_tmp -type f -name "*.gt"`; do
 		mkdir -p $out/$tmp_out/astral
 	fi
 	if [  -s $out/$tmp_out/astral/distance.d_astral_tree.nwk ]; then 
-	printf " mkdir -p ./$out/$tmp_out/astral;  /usr/bin/time -po ./$out/$tmp_out/astral/$f_tmp-log.info java -Xmx2000M -jar $DIR_Astral/astral.4.8.3.jar -t 2 -i $file -o ./$out/$tmp_out/astral/astral_tree.nwk -q $out/$tmp_out/astral/distance.d_astral_tree.nwk;cp -r ./$out/$tmp_out/astral/* $out/$tmp_out/astral/ \n">>tasks.masive
+	printf " mkdir -p ./$out/$tmp_out/astral;  /usr/bin/time -po ./$out/$tmp_out/astral/$f_tmp-log.info java -Xmx2000M -jar $DIR_Astral/astral.4.8.3.jar -t 2 -i $file -q $out/$tmp_out/astral/distance.d_astral_tree.nwk> ./$out/$tmp_out/astral/astral_tree.nwk ;cp -r ./$out/$tmp_out/astral/* $out/$tmp_out/astral/ \n">>tasks.masive
 	fi
 done
