33e7296d78832b00bbb0ccd0e11a728398f37d63
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-01-20 15:46:10 -0800

    adding the feature to write on tempdirectory

--- src/shell/mrl.sh
+++ src/shell/mrl.sh
@@ -3,34 +3,33 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export FASTMRP=~/Documents/Research/mrpmatrix
 export raxml="raxmlHPC"
-outdir=$1
+
+outdir="$(cd "$1" && pwd)"
 for x in `cat $outdir/distance-*.nwk`; do   echo $x | sed -e 's/\[&U\]//g'; done>$outdir/trees.nwk
 for x in `find $outdir -name "distance-*.nwk"`; do y=$(cat $x | sed -e 's/\[&U\]//g'); echo -n $y; echo; done > $outdir/trees.nwk
 in=$outdir/trees.nwk
 out=anchored_fastme_tree.nwk
 
 
-cd $outdir
-
-if [ -s $out ]; then
+if [ -s $outdir/$out ]; then
   echo "Ouput files exists. Refusing to rerun. "
   exit 0;
 fi
-
-tmp=`mktemp -p . mrpmaptrix.$out.XXXXX`
+cd $outdir
+tmp=`mktemp -p $outdir mrpmaptrix.$out.XXXXX`
 java -jar $FASTMRP/mrp.jar $in $tmp  PHYLIP -randomize
 
-rm -f RAxML_*$out*
+#rm -f RAxML_*$out*
+$raxml -m BINCAT -s $tmp -n $out -N 2 -p $RANDOM > /dev/null 2>&1
 
-$raxml -m BINCAT -s $tmp -n $out -N 2 -p $RANDOM
-
-test "$?" != "0" && exit 1
+#test "$?" != "0" && exit 1
 
 mv RAxML_bestTree.$out* $out
 mv RAxML_info.$out mrl.log
 
-rm RAxML_*$out* $tmp
+rm RAxML_*$out* 
 
-echo MRL Done. Output at: $out
+
+echo MRL Done. Output at: $outdir/$out
 
 
