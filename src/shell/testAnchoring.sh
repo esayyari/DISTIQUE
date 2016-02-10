2790270f4fb4753bee11b1cd87b610639e0f6bab
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-02-09 21:18:02 -0800

    typo

--- src/shell/testAnchoring.sh
+++ src/shell/testAnchoring.sh
@@ -8,11 +8,12 @@ source $DIR/setupData.sh
 
 show_help() {
 cat << EOF
-USAGE: ${0##*/} [-h] [-g GENE TREE] [-n NUMBER OF ANCHORES] [ -o OUTPUT DIR ] 
+USAGE: ${0##*/} [-h] [-g GENE TREE] [-n NUMBER OF ANCHORES] [ -o OUTPUT DIR ] [ -r REPEATS]
 Estimate species tree using DISTIQUE  method. GNEE TREES FILE is the file of gene trees concatenated.
 	-g 	GENE TREE 		The gene tree file for which we want to infer species tree
 	-n 	NUMBER OF ANCHORES	The number of anchors 
 	-o  	OUTPUT DIR		The output directory, to copy the results
+	-r 	REPEATS			The number of repeating the experiments
 EOF
 }
 n=10
@@ -20,7 +21,7 @@ if [ $# -lt 1 ]; then
 	show_help
 	exit 0;
 fi  
-while getopts "hg:n:o:" opt; do
+while getopts "hg:n:o:r:" opt; do
 	case $opt in 
 	h) 
 		show_help
@@ -34,14 +35,25 @@ while getopts "hg:n:o:" opt; do
 	o)
 		o=$OPTARG
 		;;
+	r) 
+		rt=$OPTARG
+		;;
 	esac
 done
 
+out_final=`mktemp -d`
+END=$rt
+START=1
+for (( c=$START; c<=$END; c++ ))
+do
 out=`mktemp -d`
-$WS_LOC_PUTIL/testAnchoring.py -g $gt -o $out -n $nt 
+$WS_LOC_PUTIL/testAnchoring.py -g $gt -o $out -n $nt -r $rt
 $WS_LOC_SH/mrl.sh $out
 cat $out/distance-*.nwk > $out/anchored_trees
 cat $out/anchored_mrl_tree.nwk >> $out/anchored_trees
-tar czvf  $out/results.tar.gz $out/*
-mv $out/results.tar.gz $o
+tar czvf  $out/results$rt-$c.tar.gz $out/*
+mv $out/results$rt-$c.tar.gz $out_final
+done
+tar czvf $out_final/results$rt.tar.gz $out_final/*
+mv $out_final/results$rt.tar.gz $o
 rm -r $out
