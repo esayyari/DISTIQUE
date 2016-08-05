f5f2a75bb20a12d237444911e7bdcf15847ffaff
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-08-05 01:53:42 -0700

    removing unrelated files from master

--- src/shell/runAnchoringASTRAL-all.sh
+++ src/shell/runAnchoringASTRAL-all.sh
@@ -44,24 +44,31 @@ WS_LOC_UTIL=$WS_HOME/DISTIQUE/src/utils/
 tmpDIR=`mktemp -d`
 echo $tmpDIR
 
+m=(1000 200 50)
+for n in ${m[@]}; do
 gt1000half=`mktemp  ${tmpDIR}/genetrees"$n".half.XXXXX` || exit 1
 
 head -n $n $g > $gt1000half
 sp=$dir/s_tree.trees
-
+quart=$(find $dir/distique-all/distique-all/"$n".genes.half* -name "quartet_tmp.q" || exit 1)
 
 res1000half=`mktemp -d $tmpDIR/"$n"genes.half.XXXXX` || exit 1
 
 if [ -s $gt1000half ]; then
-/usr/bin/time -p $WS_LOC_UTIL/distique.py -t 1 -m prod -a mean -g $gt1000half -o $res1000half  > $res1000half/results.log 2>&1
+/usr/bin/time -p $WS_LOC_UTIL/distique.py -t 1 -m prod -a mean -l freq -g $gt1000half -f $quart -o $res1000half  > $res1000half/results.log 2>&1
+echo "/usr/bin/time -p $WS_LOC_UTIL/distique.py -t 1 -m prod -a mean -l freq -g $gt1000half -f $quart -o $res1000half" >> $res1000half/results.log
+DATE=`date +%Y-%m-%d:%H:%M:%S`;  info=$(cd ${WS_HOME}/DISTIQUE; git show-ref  --head --tags);
+echo $DATE >> $res1000half/results.log
+echo version info: >> $res1000half/results.log
+echo $info >> $res1000half/results.log
 echo "working on $dir/estimatedgenetre.halfresolved$n has been finished!"
 fi
-
-for x in `find $tmpDIR -name "distance.*"`; do
+done
+for x in `find $tmpDIR -name "distique_*"`; do
 	y=$(dirname $x)
-	$WS_HOME/DISTIQUE/src/shell/compare.tree.sh -s $sp -g $x > $y/results-distique-all.score
+	$WS_HOME/DISTIQUE/src/shell/compare.tree.sh -s $sp -g $x > $y/results-score.sc
 done
 
 y=$(echo $o | sed -e 's/^.*\///')
-tar czf $o/testDISTIQUE-all-$n-$z-fastme.tar.gz $tmpDIR/* 
+tar czf $o/testDISTIQUE-all-BalME-fastme.tar.gz $tmpDIR/* 
 rm -r $tmpDIR
