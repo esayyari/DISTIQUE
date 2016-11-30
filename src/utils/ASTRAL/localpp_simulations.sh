7497fe580ad2b97dfe20673cb5a319a103bf9f08
esayyari <esayyari@eng.ucsd.edu>
2016-11-30 13:09:48 -0800

    adding some system.err to print all the localpp values

--- src/utils/ASTRAL/localpp_simulations.sh
+++ src/utils/ASTRAL/localpp_simulations.sh
@@ -3,7 +3,7 @@ DIR=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 set -x
 show_help() {
 cat << EOF
-USAGE: ${0##*/} [-h] [-p FILEDIRECTORY] [-g GENETREES FILENAME ] [ -o OUTPUT FOLDER ] [ -x TRUE SPECIES TREE ] [ -s ESTIMATED SPECIES TREE ]
+USAGE: ${0##*/} [-h] [-p FILEDIRECTORY] [-g GENETREES FILENAME ] [ -o OUTPUT FOLDER ] [ -x TRUE SPECIES TREE ] [ -s ESTIMATED SPECIES TREE ] [-d QUARTET RESOLUTION] [-m SUMMERIZATION METHOD]
 Generates a pool of branches with their posterior probabilities using Astral.
 
 	-h	HELP			     display help and exit
@@ -12,16 +12,19 @@ Generates a pool of branches with their posterior probabilities using Astral.
 	-o 	OUTPUT FOLDER	             The output folder to put the results there.
 	-x	TRUE SPECIES TREE	     The true species tree filename.
 	-s 	ESTIMATED SPECIES TREE 	     The estimated species tree filename.
+	-d	QUARTET RESOLUTION	     The depth of quartets to be annotated by the ASTRAL
+	-m 	SUMMERIZATION METHOD	     The method to summerize the localpp values
 EOF
 }
 
-if [ $# -ne 5 ]; then 
+if [ $# -lt 10 ]; then 
 	show_help
 	exit 1 
 fi
 
-
-while getopts "hp:g:o:x:s:" opt; do
+d=0;
+m=0;
+while getopts "hp:g:o:x:s:d:m:" opt; do
         case $opt in
         h)
                 show_help
@@ -42,6 +45,12 @@ while getopts "hp:g:o:x:s:" opt; do
 	x)
 		x=$OPTARG
 		;;
+	d)
+		d=$OPTARG
+		;;
+	m)
+		m=$OPTARG
+		;;
 	?)
 		show_help
 		exit 1;
@@ -61,18 +70,18 @@ tsp=$TmpFolder/true_species_tree.trees
 tspStat=$TmpFolder/true_species_tree.stat
 tspScored=$TmpFolder/scored_true_species_tree.trees
 tmptmp=`mktemp`
+tmpmysp=`mktemp`
 
 cp $p/$s $sp
 cp $p/$g $gt
 cp $p/$x $tsp
 
-java -Xmx4000M -jar $WS_HOME/ASTRAL/astral.$version.jar -i $gt -q $sp -t 6 > $spScored  2>$spStat ;
+java -Xmx4000M -jar $WS_HOME/ASTRAL/astral.$version.jar -i $gt -q $sp -t 6 -j $d --summerization-method $m > $spScored  2>$spStat ;
 test "$?" -ne 0 && echo "error raised in computing bipartitions and quadripartitions info of estimated species tree" && cp $spStat $o && exit 1
 echo "bipartitions and quartetpartition info of estimated species tree have been generated"
-
-java -jar $WS_HOME/ASTRAL/astral.$version.jar -i $tsp -q $tsp -t 6 >> $tspScored 2>>$tspStat;
+java -jar $WS_HOME/ASTRAL/astral.$version.jar -i $tsp -q $tsp -t 6 > $tspScored 2>$tspStat;
 test "$?" -ne 0 && echo "error raised in computing bipartition and quadripartitions info of true species tree" && cp $tspStat $o && exit 1
-cat $tspStat | grep "\{" > $tmptmp
+cat $tspStat | grep "^{" > $tmptmp
 awk 'NR %3 == 1' $tmptmp > $tspStat
 echo "bipartitions and quadripartitions info of true species tree have been generated"
 
@@ -80,7 +89,7 @@ res=$TmpFolder/ppOfBranches_species_tree
 $DIR/extractPPofPoolOfBranches.py -i $spStat -s $tspStat -o $res
 echo "pp of branches computed"
 
-tar czvf $out/ppAnalysis-$version.tar.gz $TmpFolder
+tar czvf $o/ppAnalysis-$version.tar.gz $TmpFolder
 
 rm -r $TmpFolder
 rm $tmptmp
